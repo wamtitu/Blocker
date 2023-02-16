a70e3ac70b315a9981e64d7b8d307271dacc9ccb
Alvin Kiveu <67068458+alvin-kiveu@users.noreply.github.com>
2023-02-16 03:41:52 +0300

    fanal - chages

--- background.js
+++ background.js
@@ -2,6 +2,7 @@ chrome.runtime.onInstalled.addListener(function() {
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
     // Check if the URL matches any of the pornographic websites in the blacklist
+    //INTRODUCE A JSON FILE WITH THE LIST OF URLS TO BE BLOCKED
     if (details.url.match(/umeskiasoftwares\.com|instagram\.com/)) {
       // Block the request
       return {cancel: true};
